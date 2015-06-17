5304a4ce859593ac7605e79a82bf9f36440aa3ee
102207429 <102207429@nccu.edu.tw>
2015-06-17 11:16:46 +0800

    Update member.rb

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,14 @@
 class Member < ActiveRecord::Base
 	self.table_name="b01b1_memberno"
+	query = <<-SQL
+	Select company_no, count(activity_seq) as activity_count
+	from b01a2_activity, b01b1_memberno
+	where Service_Id='B10BF05-S2' and b01a2_activity.member_no=b01b1_memberno.member_no and activity_time>='2013-01-05 00:00:00'
+	group by company_no
+	order by count(activity_seq) desc
+	limit 10
+    SQL
+
+    self.find_by_sql(query)
+  end
 end
